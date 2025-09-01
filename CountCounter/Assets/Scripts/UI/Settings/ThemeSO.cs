4370a83d3eca83cbbe2d3bffe2f2721bb665e496
Caramajau <90894366+Caramajau@users.noreply.github.com>
2025-09-01 16:37:28 +0200

    Add tooltips to theme SO

--- CountCounter/Assets/Scripts/UI/Settings/ThemeSO.cs
+++ CountCounter/Assets/Scripts/UI/Settings/ThemeSO.cs
@@ -2,30 +2,35 @@ using UnityEngine;
 
 namespace UI.Settings
 {
-    [CreateAssetMenu(menuName = "UI/Theme")]
+    [CreateAssetMenu(menuName = "ScriptableObjects/Theme")]
     public class ThemeSO : ScriptableObject
     {
-        [Header("General Colours")]
         [field: SerializeField]
+        [Tooltip("The background colour of the UI.")]
         public Color BackgroundColour { get; private set; } = Color.white;
 
         [field: SerializeField]
+        [Tooltip("The text colour of the UI.")]
         public Color TextColour { get; private set; } = Color.black;
 
-        [Header("Button Colours")]
         [field: SerializeField]
+        [Tooltip("The colour of buttons in their normal state.")]
         public Color ButtonNormal { get; private set; } = Color.white;
 
         [field: SerializeField]
+        [Tooltip("The colour of buttons when highlighted.")]
         public Color ButtonHighlighted { get; private set; } = new(0.9607843f, 0.9607843f, 0.9607843f, 1);
 
         [field: SerializeField]
+        [Tooltip("The colour of buttons when selected.")]
         public Color ButtonSelected { get; private set; } = new(0.9607843f, 0.9607843f, 0.9607843f, 1);
 
         [field: SerializeField]
+        [Tooltip("The colour of buttons when pressed.")]
         public Color ButtonPressed { get; private set; } = new(0.7843137f, 0.7843137f, 0.7843137f, 1);
 
         [field: SerializeField]
+        [Tooltip("The colour of buttons when disabled.")]
         public Color ButtonDisabled { get; private set; } = new(0.7843137f, 0.7843137f, 0.7843137f, 0.5019608f);
     }
 }
