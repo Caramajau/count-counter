c410aeaf66cd46def45adb4238da02144ab95f27
Caramajau <90894366+Caramajau@users.noreply.github.com>
2025-09-01 16:40:48 +0200

    Remove redundant ones in Theme SO

--- CountCounter/Assets/Scripts/UI/Settings/ThemeSO.cs
+++ CountCounter/Assets/Scripts/UI/Settings/ThemeSO.cs
@@ -19,15 +19,15 @@ namespace UI.Settings
 
         [field: SerializeField]
         [Tooltip("The colour of buttons when highlighted.")]
-        public Color ButtonHighlighted { get; private set; } = new(0.9607843f, 0.9607843f, 0.9607843f, 1);
+        public Color ButtonHighlighted { get; private set; } = new(0.9607843f, 0.9607843f, 0.9607843f);
 
         [field: SerializeField]
         [Tooltip("The colour of buttons when selected.")]
-        public Color ButtonSelected { get; private set; } = new(0.9607843f, 0.9607843f, 0.9607843f, 1);
+        public Color ButtonSelected { get; private set; } = new(0.9607843f, 0.9607843f, 0.9607843f);
 
         [field: SerializeField]
         [Tooltip("The colour of buttons when pressed.")]
-        public Color ButtonPressed { get; private set; } = new(0.7843137f, 0.7843137f, 0.7843137f, 1);
+        public Color ButtonPressed { get; private set; } = new(0.7843137f, 0.7843137f, 0.7843137f);
 
         [field: SerializeField]
         [Tooltip("The colour of buttons when disabled.")]
