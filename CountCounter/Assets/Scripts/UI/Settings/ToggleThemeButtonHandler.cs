08e34be3e6a8978462f76e5274f0bb0a7e1a0fff
Caramajau <90894366+Caramajau@users.noreply.github.com>
2025-09-01 16:50:31 +0200

    Create handler for toggle theme button

---
+++ CountCounter/Assets/Scripts/UI/Settings/ToggleThemeButtonHandler.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Settings
+{
+    public class ToggleThemeButtonHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private Button toggleThemeButton;
+
+        [SerializeField] 
+        private ThemeSO lightTheme;
+
+        [SerializeField] 
+        private ThemeSO darkTheme;
+
+        private bool isDarkMode = false;
+
+        private void OnEnable()
+        {
+            toggleThemeButton.onClick.AddListener(ToggleTheme);
+        }
+
+        private void OnDisable()
+        {
+            toggleThemeButton.onClick.RemoveListener(ToggleTheme);
+        }
+
+        private void ToggleTheme()
+        {
+            isDarkMode = !isDarkMode;
+            ThemeManager.Instance.ApplyTheme(isDarkMode ? darkTheme : lightTheme);
+        }
+    }
+}
