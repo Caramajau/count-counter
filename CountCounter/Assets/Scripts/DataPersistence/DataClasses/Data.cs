46f8b1748540f039c14c7517f4d0855a097671ea
Caramajau <90894366+Caramajau@users.noreply.github.com>
2025-09-28 18:59:29 +0200

    Create foundation for the persistence system

---
+++ CountCounter/Assets/Scripts/DataPersistence/DataClasses/Data.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace DataPersistence.DataClasses
+{
+    [System.Serializable]
+    public abstract class Data
+    {
+        // The fields need to be SerializeField or public.
+        // Intentionally not using [field: SerializeField] to make data more readable.
+        // Might change in the future.
+        [SerializeField]
+        private long lastUpdated;
+        public long LastUpdated { get { return lastUpdated; } set { lastUpdated = value; } }
+    }
+}
---
+++ CountCounter/Assets/Scripts/DataPersistence/IDataPersistence.cs
@@ -0,0 +1,10 @@
+using DataPersistence.DataClasses;
+
+namespace DataPersistence
+{
+    public interface IDataPersistence<T> where T : Data, new()
+    {
+        void LoadData(T data);
+        void SaveData(T data);
+    }
+}
