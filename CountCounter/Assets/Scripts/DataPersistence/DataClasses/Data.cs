61cd852a34246ac7e2033e820ab342246a87316b
Caramajau <90894366+Caramajau@users.noreply.github.com>
2025-09-30 16:12:47 +0200

    Change lastupdated to use auto properties

--- CountCounter/Assets/Scripts/DataPersistence/DataClasses/Data.cs
+++ CountCounter/Assets/Scripts/DataPersistence/DataClasses/Data.cs
@@ -5,11 +5,7 @@ namespace DataPersistence.DataClasses
     [System.Serializable]
     public abstract class Data
     {
-        // The fields need to be SerializeField or public.
-        // Intentionally not using [field: SerializeField] to make data more readable.
-        // Might change in the future.
-        [SerializeField]
-        private long lastUpdated;
-        public long LastUpdated { get => lastUpdated; set => lastUpdated = value; }
+        [field: SerializeField]
+        public long LastUpdated { get; set; }
     }
 }
